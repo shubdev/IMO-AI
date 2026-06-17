0fe367a82ecb3e4bf66c7016950247aeecb730ef
shubdev <shub.mahadule@gmail.com>
2026-06-17 12:12:42 +0530

    feat: initialize Express application and server entry point with middleware and routes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   const buildPath = path.resolve(__dirname, "../frontend/dist");
   app.use(express.static(buildPath));
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
   res.sendFile(path.join(buildPath, 'index.html'));
 });
 }
